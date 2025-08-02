294314e7092f7527e76ed4827ff896c024c77e25
Esteban Dev <esteban.bevf@gmail.com>
2025-08-02 17:00:06 -0500

    Mejorando portfolio y actualizando rutas

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:EstebanV23/portfolio.git main:gh-pages
+# git push -f git@github.com:estevandev23/portfolio.git main:gh-pages
 
 cd -
